87aae6cbbe0e5c8d348917dded478836ee3fcac0
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-04-28 16:07:30 -0300

    Create project Screen_Match

---
+++ src/main/java/br/com/yurifont/sreenmatch/SreenmatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.yurifont.sreenmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreenmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreenmatchApplication.class, args);
+	}
+
+}
