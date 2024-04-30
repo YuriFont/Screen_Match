5ff05573f33780f2ac60ba2ee47c5fccf8d3f8e8
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-04-29 23:40:22 -0300

    Create a metod getData for class ConsumeAPI

--- src/main/java/br/com/yurifont/sreenmatch/service/ConsumeAPI.java
+++ src/main/java/br/com/yurifont/sreenmatch/service/ConsumeAPI.java
@@ -1,5 +1,27 @@
 package br.com.yurifont.sreenmatch.service;
 
-public class ConsumeAPI {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+public class ConsumeAPI {
+    public String getData(String url) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String json = response.body();
+        return json;
+    }
 }
