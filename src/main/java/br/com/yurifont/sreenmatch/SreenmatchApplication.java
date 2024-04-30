a508d36c06485f8c332c53f9dc977ac768866406
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-04-29 23:22:40 -0300

    Create package service and class consumeAPI

--- src/main/java/br/com/yurifont/sreenmatch/SreenmatchApplication.java
+++ src/main/java/br/com/yurifont/sreenmatch/SreenmatchApplication.java
@@ -1,13 +1,18 @@
 package br.com.yurifont.sreenmatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SreenmatchApplication {
+public class SreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
