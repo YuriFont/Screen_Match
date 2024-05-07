9bb6b2c476ead1fc2669d9df956621f00c7e813f
YuriFont <yurifontenelesilva180303@gmail.com>
2024-05-06 22:43:50 -0300

    Create package model and record SeriesData

---
+++ src/main/java/br/com/yurifont/sreenmatch/model/SeriesData.java
@@ -0,0 +1,9 @@
+package br.com.yurifont.sreenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record SeriesData(@JsonAlias("Title") String title,
+                         String imdbRating,
+                         String totalSeasons) {
+
+}
